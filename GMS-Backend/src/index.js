561ba1bc26eda66e93f52e323c72cff4507c13ba
BryanVK <bryanviriyak@gmail.com>
2025-04-09 11:32:09 +0700

    update alamat

--- GMS-Backend/src/index.js
+++ GMS-Backend/src/index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://82.112.227.86:5174', // alamat frontend kamu
+  origin: '*', // alamat frontend kamu
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }));
