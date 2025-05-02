ad0c34e9a49b7cb10fcdb90b5ebe1a793e3c7889
BryanVK <bryanviriyak@gmail.com>
2025-05-02 14:26:04 +0700

    uodate

--- Gemma-Management-System/src/App.jsx
+++ Gemma-Management-System/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Login } from './components/Login';
 import { Menu } from './components/Menu';
@@ -10,32 +10,14 @@ function App() {
 
   useEffect(() => {
     const storedLoginStatus = localStorage.getItem("isLoggedIn");
-    const expiredAt = localStorage.getItem("expiredAt");
-
-    if (storedLoginStatus === "true" && expiredAt) {
-      const now = new Date();
-      const expiredTime = new Date(expiredAt);
-
-      if (now < expiredTime) {
-        setIsLoggedIn(true);
-        const timeout = expiredTime.getTime() - now.getTime();
-        const timer = setTimeout(() => {
-          handleLogout();
-          alert("Sesi login berakhir, silakan login kembali.");
-        }, timeout);
-        return () => clearTimeout(timer);
-      } else {
-        handleLogout();
-      }
+    if (storedLoginStatus === "true") {
+      setIsLoggedIn(true);
     }
   }, []);
 
   const handleLogin = () => {
     setIsLoggedIn(true);
     localStorage.setItem("isLoggedIn", "true");
-    const expiredAt = new Date();
-    expiredAt.setMinutes(expiredAt.getMinutes() + 10);
-    localStorage.setItem("expiredAt", expiredAt.toISOString());
   };
 
   const handleLogout = () => {
