4b982043f1a4d6ccfed9ab5cb6df4282527bc3c7
BryanVK <bryanviriyak@gmail.com>
2025-05-09 16:16:50 +0700

    update history

--- Gemma-Management-System/src/components/HistoryOnCall.jsx
+++ Gemma-Management-System/src/components/HistoryOnCall.jsx
@@ -41,10 +41,10 @@ export function HistoryOnCall({ client, onClose }) {
                     History OnCall - No: {client.no}
                 </h2>
                 <h3 className="text-xl text-center mb-6">
-                    SN: {client.serial} \n
-                    Mesin: {client.model} \n
-                    Cabang: {client.namacabang} \n
-                    Teknisi: {client.teknisi} \n
+                    SN: *{client.serial}* 
+                    , Mesin: *{client.model}*
+                    , Cabang: *{client.namacabang}*
+                    , Teknisi: *{client.teknisi}*
                 </h3>
 
                 <div className="overflow-x-auto">
